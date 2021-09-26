c1bf2e20866a4d841b6e648fafd655e66d8656a8
vofm <valerafm.vofm@gmail.com>
2021-09-26 22:40:39 +0300

    Add css

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -38,8 +38,8 @@
 // comments - количество комментариев
 // downloads - количество загрузок
 
-export function fetchImages(image_type, orientation, query, page, per_page, key) {
-  return fetch(`https://pixabay.com/api/?image_type=${image_type}&orientation=${orientation}&q=${query}&page=${page}&per_page=${per_page}&key=${key}`)
+export function fetchImages(image_type, query, page, per_page, key) {
+  return fetch(`https://pixabay.com/api/?image_type=${image_type}&q=${query}&page=${page}&per_page=${per_page}&key=${key}`)
     .then(response => {
       if (response.status !== 200) {
         throw new Error(response.status);
--- src/js/main.js
+++ src/js/main.js
@@ -3,56 +3,55 @@ import templateGallery from "../templates/gallery.hbs";
 import templateImage from "../templates/image.hbs";
 import { fetchImages } from "./apiService.js";
 
-// Форма поиска
-// Создает DOM-элемент следующей структуры. Можно использовать шаблонизацию.
+const debounce = require('lodash.debounce');
+
 const bodyRef = document.querySelector('body');
 
 bodyRef.insertAdjacentHTML('afterbegin', templateForm());
 
-// Галерея изображений
-// Создает DOM-элемент следующей структуры.
 const formRef = bodyRef.querySelector('#search-form');
 
 formRef.insertAdjacentHTML('afterend', templateGallery());
 
-// Кнопка 'Load more'
-// При нажатии на кнопку Load more должна догружаться следующая порция изображений и рендериться вместе с предыдущими.
-
-// Страница должна автоматически плавно проскроливаться после рендера изображений,
-// чтобы перевести пользователя на следующие загруженные изображения.
-// Используй метод Element.scrollIntoView().
-
 const element = document.getElementById('my-element-selector');
 
 function scrollView() {
-    console.log(element);
     element.scrollIntoView({block: "end", behavior: "smooth"});
 }
 
-// Дополнительно
-// Можно добавить плагин нотификаций, например pnotify, и показывать нотификации на результат HTTP-запросов
-// Можно добавить функционал отображения большой версии изображения через плагин модального окна, например basicLightbox, при клике на изображение галереи
-// Вместо кнопки Load more можно сделать бесконечную загрузку при скроле используя Intersection Observer.
-
 const getGalleryRef = bodyRef.querySelector('.gallery');
-
-
-console.log(getGalleryRef);
 
 const options = {
     image_type: 'photo',
-    orientation: '',
+    query: '',
     page: 0,
     per_page: 12,
     key: '8645843-73f0b565a99dd2126325d1c4b',
 }
-let { image_type, orientation, query, page, per_page, key } = options;
+let { image_type, query, page, per_page, key } = options;
+let bgrToggle = 1;
 
 function getImage(e) {
-    fetchImages(image_type, orientation, e.target.value, page=1, per_page, key)
+    if (e.target.value === '' || e.target.value === " ") {
+        getGalleryRef.innerHTML = '';
+        bodyRef.classList.toggle("bgr");
+        bgrToggle = 1;
+        return;
+    }
+    if (bgrToggle === 1) {
+        bodyRef.classList.toggle("bgr");
+        bgrToggle = 0;
+    }
+    fetchImages(image_type, query = e.target.value, page = 1, per_page, key)
         .then(images => {
-            console.log(images);
-            const markup = images.hits.map((img) => { return templateImage(img)}).join("") + '<li class="last-item"><p class="buttonLoadMore">Load more></p></li>';
+            if (images.hits.length === 0) {
+                if (bgrToggle === 0) {
+                    bodyRef.classList.toggle("bgr");
+                    bgrToggle = 1;
+                    }
+                return;
+            }
+            const markup = images.hits.map((img) => { return templateImage(img)}).join("") + '<li class="last-item"><p class="buttonLoadMore"></p></li>';
             getGalleryRef.innerHTML = markup;
             const buttonLoadMoreRef = bodyRef.querySelector('.buttonLoadMore');
             buttonLoadMoreRef.addEventListener('click', getLoadMore);
@@ -60,19 +59,13 @@ function getImage(e) {
 };
 
 const searchImageRef = formRef.querySelector('.search-input');
-searchImageRef.addEventListener('input', getImage);
-
-
-
-// buttonLoadMoreRef.addEventListener('click', scrollView);
+searchImageRef.addEventListener('input', debounce(getImage, 500));
 
 function getLoadMore() {
-    fetchImages(image_type, orientation, 'cat', page += 1, per_page, key)
+    fetchImages(image_type, query, page += 1, per_page, key)
         .then(images => {
             const getGalleryLastItemRef = getGalleryRef.querySelector('.last-item');
-            console.log(images);
             const markup = images.hits.map((img) => { return templateImage(img) }).join("");
-            console.log(markup);
             getGalleryLastItemRef.insertAdjacentHTML("beforebegin", markup);
             scrollView();
         })
